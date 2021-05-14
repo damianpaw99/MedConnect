d33a75f62c915148473f3f6a3145648ea0bda21f
damiapaw99 <damianpaw99@gmail.com>
2021-05-14 19:50:53 +0200

    Initial commit

---
+++ src/main/java/edu/ib/MedconnectApplication.java
@@ -0,0 +1,13 @@
+package edu.ib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedconnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/ib/MedconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedconnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
